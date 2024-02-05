42c50f152ecf60726abeab72fb3621ffe7baf2fc
mclk623 <66455829+mclk623@users.noreply.github.com>
2024-02-05 22:40:10 +0800

    Update PlayerMoveListener.java

--- src/main/java/com/iridium/iridiumskyblock/listeners/PlayerMoveListener.java
+++ src/main/java/com/iridium/iridiumskyblock/listeners/PlayerMoveListener.java
@@ -27,4 +27,3 @@ public class PlayerMoveListener implements Listener {
             for (ItemStack item : event.getPlayer().getInventory().getContents()) {
                 // ... （后续代码）
             }
-}
